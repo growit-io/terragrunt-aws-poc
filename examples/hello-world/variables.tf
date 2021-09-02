cffa867a4a76945e0520ff47d7b2706fab958064
Uwe Stuehler <ustuehler@growit.io>
2021-09-02 15:43:15 +0200

    style(terragrunt): rewrite `*.tf` and `terragrunt.hcl` files to the canonical format

--- examples/hello-world/variables.tf
+++ examples/hello-world/variables.tf
@@ -1,29 +1,29 @@
 variable "root" {
   description = "The name of the first subdirectory under to the root directory, leading to the current Terragrunt configuration directory."
-  type = string
+  type        = string
 
   validation {
-    condition = var.root == "examples"
+    condition     = var.root == "examples"
     error_message = "The value should match the name of the first subdirectory that leads to this module, under the root directory of the repository. The expected name for this directory is \"examples\"."
   }
 }
 
 variable "example" {
   description = "The name of the first subdirectory under the \"examples\" directory in this repository."
-  type = string
+  type        = string
 
   validation {
-    condition = var.example == "hello-world"
+    condition     = var.example == "hello-world"
     error_message = "The value should match the name of the directory in which this variables.tf file is located."
   }
 }
 
 variable "hello" {
   description = "An example input variable defined in the `inputs` attribute of a terragrunt.yml file."
-  type = string
+  type        = string
 
   validation {
-    condition = var.hello == "world"
+    condition     = var.hello == "world"
     error_message = "The value is expected to be set to \"world\", somewhere in one of the terragrunt.yml files in the parent directory hierarchy."
   }
 }
