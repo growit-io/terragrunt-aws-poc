50ee1e0e85793138f5f62af137cccb7375267903
Uwe Stuehler <ustuehler@growit.io>
2021-09-03 21:47:01 +0200

    refactor(terragrunt-aws-poc): fix variable type definitions for access and identity root modules

--- modules/poc/aws-management-iam-identity/variables.tf
+++ modules/poc/aws-management-iam-identity/variables.tf
@@ -1,18 +1,21 @@
 variable "users" {
-  type = map(object({
-    access_key = bool
-    groups     = set(string)
-    path       = string
-    pgp_key    = string
-  }))
-
+  type        = any
   default     = {}
-  description = "A map of objects describing the IAM users to create in the AWS Organizations management account."
+  description = <<-EOT
+    A map of objects describing the IAM users to create in the AWS Organizations management account.
+
+    The map keys are the names of the IAM users to create and the values support the following attributes:
+
+    - **access_key** (`bool`, default: `false`): Whether to create an access key for the user.
+    - **groups** (`set(string)`, default: `[]`): The names of IAM groups which the user should be a member of. Each name must correspond to a map key in the `groups` variable.
+    - **path** (`string`, default: `"/"`): The IAM path under which to create the user.
+    - **pgp_key** (`string`, default: `null`): A base64-encoded PGP public key to encrypt the secret access key with. If a PGP key is specified, then the resulting access key will not be directly usable by other Terraform root modules and the secret key will be returned in the `encrypted_access_keys` output, instead of in the `plaintext_access_keys` output.
+  EOT
 }
 
 variable "groups" {
   type        = set(string)
-  description = "A set of names of IAM groups to create in the AWS Organizations management account."
+  description = "A set of names of IAM groups that are assumed to exist in the AWS Organizations management account."
 }
 
 variable "default_region" {
